2a9c6f0fc26022a23af286ccbb7a21ac414327f0
chrisbrigs9001 <chrisbrigs9001@gmail.com>
2026-02-02 17:54:42 -0600

    before gen page

--- src/main.py
+++ src/main.py
@@ -1,13 +1,36 @@
 from textnode import *
 import os
+import shutil
+
+
 
 def main():
-    new_obj = TextNode("fuck", TextType.BOLD, "meinthe.ass")
-    print(new_obj)
+    #new_obj = TextNode("fuck", TextType.BOLD, "meinthe.ass")
+    #print(new_obj)
     src = "./static"
     dst = "./public"
+    print("Deleting public directory...")
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+
+    print("Copying static files to public directory...")
+    copy_content(src, dst)
     
-def copy_content(source, dest):
-    pass
+    
+    
+    
+def copy_content(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_content(from_path, dest_path)
+    
     
 main()
